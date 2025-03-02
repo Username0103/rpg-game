35238cb65ec841138d4516c5d327ea25f0eedf08
Username0103 <49798750+Username0103@users.noreply.github.com>
2025-03-02 20:22:52 -0300

    slightly changed formatting

--- config_manager.py
+++ config_manager.py
@@ -6,17 +6,17 @@ import os
 
 config_parser = configparser.ConfigParser()
 
-def _get_path(file: str = 'config.ini'):
+def _get_path(file: str = 'config.ini') -> str:
     script_dir = os.path.dirname(os.path.abspath(__file__))
     config_file_path = os.path.join(script_dir, file)
     return config_file_path
 
-def _read_config():
+def _read_config() -> configparser.ConfigParser:
     config_file_path = _get_path()
     config_parser.read(config_file_path)
     return config_parser
 
-def reset_config():
+def reset_config() -> None:
     defaults_path = _get_path('config_defaults.ini')
     working_path = _get_path()
     with open(defaults_path, encoding = 'utf_8') as defaults:
@@ -30,22 +30,22 @@ def _check_presence(to_check: tuple[str, dict]) -> bool:
     except configparser.NoOptionError:
         return False
     except configparser.NoSectionError as exc:
-        raise KeyError(f'received wrong section write_config(),\
-                        was {to_check[0]}') from exc
+        raise KeyError('received wrong section to write_config(),'
+                       f'was {to_check[0]}') from exc
     return True
 
-def write_config(to_write: tuple[str, dict]):
+def write_config(to_write: tuple[str, dict]) -> None:
     if _check_presence(to_write) is False:
         raise KeyError('Tried to write config to non-existing configuration!')
     config_parser[to_write[0]].update(to_write[1])
     with open(_get_path(), 'w', encoding = 'utf_8') as config_file:
         config_parser.write(config_file)
 
-def get_dict_section(section):
+def get_dict_section(section) -> dict[str, str]:
     config = _read_config()
     return dict(config[section])
 
-def get_config(to_get: tuple[str, str]):
+def get_config(to_get: tuple[str, str]) -> str:
     config = _read_config()
     result = config.get(to_get[0], to_get[1])
     return result
